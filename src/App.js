851abad48fd6fc0b876d1e772feb301134be4522
faizan <faizanahmad72560@gmail.com>
2023-06-13 15:17:35 +0530

    changesdone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import pic from './pic.jpeg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div style={{backgroundColor:"white",height:"40vh",width:"20vw",position:"absolute",right:"20vw",top:"25vh"}}>
+        <div>
+        <div style={{display:"flex",justifyContent:"space-between"}}>
+          <p style={{color:"yellow"}}>ALREADY MEMBERS</p>
+          <p style={{color:"grey"}}>Need Help?</p>
+
+
+        </div>
+
+        <div style={{display:"flex",justifyContent:"center",margin:"10px"}}>
+          <input placeholder='Mahisa Dyan Diptya' style={{height:"6vh",width:"20vw",backgroundColor:"#F7F9F9"}}></input>
+        </div>
+        <div style={{display:"flex",justifyContent:"center",margin:"10px"}}>
+          <input placeholder='Enter your password' style={{height:"6vh",width:"20vw",backgroundColor:"#F7F9F9"}}></input>
+        </div>
+        <div style={{display:"flex",justifyContent:"center",margin:"10px"}}>
+          <button style={{height:"6vh",width:"20vw",backgroundColor:"#205A63"}}>Sign in</button>
+        </div>
+
+      </div>
+
+      </div>
+      <div style={{display:"flex", justifyContent:"center",alignItems:"center",flexDirection:"column",position:"absolute",right:"23vw",top:"70vh"}}>
+        <div>Dont have an Account yet ?</div>
+        <div>Create an Account</div>
+
+      </div>
+
     </div>
   );
 }
